4bd07ac7fb8f1b81496500bd67dae8d01b10a113
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2024-01-17 17:38:09 +0300

    IndieBrew

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const navbar = document.querySelector('.navbar');
+
 const menuBtn = document.querySelector('.navbar .menu_btn i');
 const mobileMenu = document.querySelector('.navbar .nav_items');
 const menuItems = document.querySelectorAll('.navbar .nav_items a');
+
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('active');
@@ -14,13 +15,20 @@ menuItems.forEach(item => {
     });
 });
 
+// Scrolling Feature
+const navbar = document.querySelector('.navbar');
 document.addEventListener('scroll', () => {
     let scrolled = window.scrollY;
 
-    if(scrolled > 10) {
+    if(scrolled > 600) {
         navbar.classList.add('shadow');
     } else {
         navbar.classList.remove('shadow');
     }
-})
+});
 
+// Year autoupdate feature
+const year = document.getElementById('year');
+const currentYear = new Date().getFullYear();
+const newYear = new Date(`january 01 ${currentYear + 1} 00:00:00`);
+year.innerText = currentYear;
