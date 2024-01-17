32ee0d47c7a4bf81003025c8c40820af29ee1e85
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2024-01-17 17:42:05 +0300

    IndieBrew

--- main.js
+++ main.js
@@ -31,4 +31,4 @@ document.addEventListener('scroll', () => {
 const year = document.getElementById('year');
 const currentYear = new Date().getFullYear();
 const newYear = new Date(`january 01 ${currentYear + 1} 00:00:00`);
-year.innerText = currentYear;
+year.innerHTML = currentYear;
